1df5c7ed4d1607dbc1459a74a70684025dc772f9
Greg Hale <imalsogreg@gmail.com>
2014-08-28 16:12:23 -0400

    Add ok_directions option to limit to xcorr to outbound eg

--- code/trode_location/field_xcorr_by_trode_location/collect_xcorr_data.m
+++ code/trode_location/field_xcorr_by_trode_location/collect_xcorr_data.m
@@ -1,9 +1,9 @@
 function [CA3CA1summary,CA1CA1summary] = collect_xcorr_data()
 
-ca1ca1Rats = {'~/Data/yolanda/120711',...  % About 50 pairs
-              '~/Data/yolanda/121111',...  % About 20 pairs
+ca1ca1Rats = {'~/Data/yolanda/120711',...  % About 50  pairs
+              '~/Data/yolanda/121111',...  % About 20  pairs
               '~/Data/yolanda/112511',...  % About 100 pairs
-              '~/Data/blue/032412'... % ?
+              '~/Data/blue/032413'...      %       8   pairs
 };
 ca3ca1Rats = {'~/Data/yolanda/120711',...  % About 30 pairs
               '~/Data/yolanda/121111',...  % About 2  pairs
