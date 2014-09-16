2dcfbca35e3a15c40efc3878911ceea6fd09f8bd
Greg Hale <imalsogreg@gmail.com>
2014-09-15 22:47:09 -0400

    Added art and figure 1 code

--- code/thesis/wave_fig/describe_wave.m
+++ code/thesis/wave_fig/describe_wave.m
@@ -1,16 +1,70 @@
-%% Load data
+function describe_wave(d,m,varargin)
 
-if(strcmp(rat,'morpheus'))
-rat_conv = morpheus_rat_conv_table;
-trode_groups = morpheus_trode_groups;
-load ~/Data/morpheus/052310/t_data.mat run_timewin pos_info place_cells eeg_r mua_r; 
-% 35 place cells on track.
-% 10mins presleep, 1hr postsleep
+[defaultChans,defaultTWin] = ratDefaults(m);
+
+p = inputParser();
+p.addParamValue('upleft_chanlabels',defaultChans);
+p.addParamValue('timewin',defaultTWin);
+p.addParamValue('eeg_r',[]);
+p.addParamValue('brainPicPath','/home/greghale/Documents/Papers/RetroProject/code/thesis/wave_fig/brainFig.png');
+p.parse(varargin{:});
+opt = p.Results;
+
+if(isempty(opt.eeg_r))
+    d.eeg_r = prep_eeg_for_regress(eeg_r);
+else
+    d.eeg_r = opt.eeg_r;
+end
+
+drawBrainAndExampleWaves(d,m,opt.brainPicPath,opt.timewin,opt.upleft_chanlabels);
+
+%dt = 0.5;
+%lfp_wave = gh_long_wave_regress(eeg_r, rat_conv, 'short_timewin', dt);
+%mua_wave = gh_long_wave_regress(mua_r, rat_conv, 'short_timewin', dt);
+
+
 end
 
 
-%% Wave parameters
+function f = drawBrainAndExampleWaves(d,m,brainPicPath,tWin,chanlabels)
 
-dt = 0.5;
-lfp_wave = gh_long_wave_regress(eeg_r, rat_conv, 'short_timewin', dt);
-mua_wave = gh_long_wave_regress(mua_r, rat_conv, 'short_timewin', dt);
+f = figure;
+subplot(1,2,1);
+image([-7.75,7.75],[-9.9,15.8], imread(brainPicPath));
+axis equal;
+
+eeg = contwin_r(contchans_r(d.eeg_r,'chanlabels',chanlabels),safeTimeWin(tWin,d.eeg));
+subplot(1,2,2);
+gh_plot_cont(eeg.raw,'spacing',0.4);
+hold on;
+gh_plot_cont(eeg.theta,'spacing',0.4,'trode_groups',d.trode_groups,'LineWidth',3);
+xlim(tWin);
+
+
+end
+
+
+function tWin = safeTimeWin(tWin, cdat)
+tWin = [max(cdat.tstart,tWin(1)-2), min(cdat.tend,tWin(2)+2)];
+end
+
+
+
+function [chanLabels,tWin] = ratDefaults(m)
+
+     if(strContains(m.basePath,'morpheus'))
+         chanLabels = {'10','15','24'};
+         tWin = [760,760.5];
+     else
+         error('ratUpLeftChans:noLabelData',['Don''t know which ',...
+                                             'channels to use for ',...
+                                             'example wave figure']);
+     end
+
+end
+
+function [b,twin] = strContains(a,b)
+    
+    b = ~isempty(regexp(a,b,'ONCE'));
+       
+end
