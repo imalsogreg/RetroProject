bd2acd61d81403bd96201d1ca3497a4669312df0
Greg Hale <imalsogreg@gmail.com>
2014-09-21 20:38:13 -0400

    points on describe-wave brain pic

--- code/thesis/wave_fig/describe_wave.m
+++ code/thesis/wave_fig/describe_wave.m
@@ -10,10 +10,12 @@ p.addParamValue('brainPicPath','/home/greghale/Documents/Papers/RetroProject/cod
 p.parse(varargin{:});
 opt = p.Results;
 
-if(isempty(opt.eeg_r))
-    d.eeg_r = prep_eeg_for_regress(eeg_r);
-else
-    d.eeg_r = opt.eeg_r;
+if ~isfield(d,'eeg_r')
+    if(isempty(opt.eeg_r))
+        d.eeg_r = prep_eeg_for_regress(eeg_r);
+    else
+        d.eeg_r = opt.eeg_r;
+    end
 end
 
 drawBrainAndExampleWaves(d,m,opt.brainPicPath,opt.timewin,opt.upleft_chanlabels);
@@ -30,19 +32,34 @@ function f = drawBrainAndExampleWaves(d,m,brainPicPath,tWin,chanlabels)
 
 f = figure;
 subplot(1,2,1);
-image([-7.75,7.75],[-9.9,15.8], imread(brainPicPath));
+image([-7.75,7.75],[9.9,-15.8], imread(brainPicPath));
+set(gca,'YDir','normal');
+hold on;
+for n = 1:numel(chanlabels)
+    [x,y,c] = trodePosAndColor(chanlabels{n},d.trode_groups, d.rat_conv_table);
+    plot(x,y,'.','Color',c,'MarkerSize',20);
+end
 axis equal;
 
-eeg = contwin_r(contchans_r(d.eeg_r,'chanlabels',chanlabels),safeTimeWin(tWin,d.eeg));
-subplot(1,2,2);
-gh_plot_cont(eeg.raw,'spacing',0.4);
-hold on;
-gh_plot_cont(eeg.theta,'spacing',0.4,'trode_groups',d.trode_groups,'LineWidth',3);
-xlim(tWin);
 
+eeg = contwin_r(contchans_r(d.eeg_r,'chanlabels',chanlabels),safeTimeWin(tWin,d.eeg));
+peaks = gh_troughs_from_phase(contchans_r(eeg,'chanlabels',chanlabels{1}));
+subplot(1,2,2);
+gh_plot_cont(eeg.raw,'spacing',-0.4);
+hold on;
+gh_plot_cont(eeg.theta,'spacing',-0.4,'trode_groups',d.trode_groups,'LineWidth',3);
+xlim(tWin);
+[xs,ys] = gh_raster_points(peaks,'y_range',[-1,0.1]);
+plot(xs,ys,'k--');
 
 end
 
+function [x,y,c] = trodePosAndColor(tName,trode_groups,rat_conv_table)
+    g = trode_group(tName,trode_groups);
+    c = g.color;
+    x = trode_conv(tName,'comp','brain_ml',rat_conv_table);
+    y = trode_conv(tName,'comp','brain_ap',rat_conv_table);
+end
 
 function tWin = safeTimeWin(tWin, cdat)
 tWin = [max(cdat.tstart,tWin(1)-2), min(cdat.tend,tWin(2)+2)];
@@ -53,8 +70,8 @@ end
 function [chanLabels,tWin] = ratDefaults(m)
 
      if(strContains(m.basePath,'morpheus'))
-         chanLabels = {'10','15','24'};
-         tWin = [760,760.5];
+         chanLabels = {'23','15','12'};
+         tWin = [822.7,823.2]; % TODO is rat running now?
      else
          error('ratUpLeftChans:noLabelData',['Don''t know which ',...
                                              'channels to use for ',...
