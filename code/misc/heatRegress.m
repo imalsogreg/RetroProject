22a522e186426176c7bea1631b55412d16622ab5
Greg Hale <imalsogreg@gmail.com>
2015-01-02 17:23:33 -0500

    tweaking heat regression

--- code/misc/heatRegress.m
+++ code/misc/heatRegress.m
@@ -1,7 +1,8 @@
 function [slope,intercept] = heatRegress(xLims,yLims,heats,varargin)
 
 p = inputParser();
-p.addParamValue('nSamples',500);
+%p.addParamValue('nSamples',5000);
+%p.addParamValue('draw',false);
 p.parse(varargin{:});
 opt = p.Results;
 
@@ -9,19 +10,19 @@ maxH = max(max(heats));
 minH = min(min(heats));
 normalizedHeats = (heats - minH) / (maxH - minH);
 
-nSampsPerBin = opt.nSamples / (numel(heats)) / sum(sum(normalizedHeats));
+nSampsPerBin = opt.nSamples / (numel(heats)) / mean(mean(normalizedHeats));
 
 nRows = size(heats,1);
 nCols = size(heats,2);
 
-rowEdges = linspace(yLims(1),yLims(2),nRows);
-dRow = rowEdges(2)-rowEdges(1);
-rowCenters = edgesToCenters(rowEdges);
-colEdges = linspace(xLims(1),xLims(2),rCols);
-dCol = colEdges(2) - colEdges(1);
-colCenters = edgesToCetners(colEdges);
+rowCenters = linspace(yLims(1),yLims(2),nRows);
+dRow = mean(diff(rowCenters));
+rowEdges = bin_edges_to_centers(rowCenters);
+colCenters = linspace(xLims(1),xLims(2),nCols);
+dCol = mean(diff(colCenters));
+colEdges  = bin_centers_to_edges(colCenters);
 
-sampsInBins = arrayfun(@(x) genSamples(x,nSampsPerBin), normalizedHeats);
+sampsInBins = arrayfun(@(x) genSamples(x,nSampsPerBin), normalizedHeats,'UniformOutput',false);
 
 sampsX = nan .* ones(1,opt.nSamples * 10);
 sampsY = sampsX;
@@ -29,26 +30,33 @@ nAccum = 0;
 for r = 1:nRows
     for c = 1:nCols
         thisNSamps = numel(sampsInBins{r,c});
-        sampsX(nAccum:(nAccum+thisNSamps)) = colCenters(c) .* ones(1,thisNSamps);
-        sampsY(nAccum:(nAccum+thisNSamps)) = rowCenters(r) .* ones(1,thisNSamps);
+        sampsX(nAccum+1:(nAccum+thisNSamps)) = colCenters(c) .* ones(1,thisNSamps);
+        sampsY(nAccum+1:(nAccum+thisNSamps)) = rowCenters(r) .* ones(1,thisNSamps);
         nAccum = nAccum + thisNSamps;
     end
 end
 sampsX = sampsX(1:nAccum);
 sampsY = sampsY(1:nAccum);
 
-sampsX = sampsX + dCol.*rand(1:numel(sampsX)) - (dCol/2);
-sampsY = sampsY + dRow.*rand(1:numel(sampsY)) - (dRow/2);
+sampsX = sampsX + dCol/5.*randn(1,numel(sampsX));
+sampsY = sampsY + dRow/5.*randn(1,numel(sampsY));
 
 b = regress(sampsY', [sampsX',ones(size(sampsX'))]);
 slope = b(1);
 intercept = b(2);
 
+if(opt.draw)
+   imagesc(xLims,yLims,heats);
+   hold on;
+   set(gca,'YDir','normal');
+   plot(sampsX,sampsY,'.');
+end
+
 end
 
 function samps = genSamples(prob, nSampsPerBin)
 
-  samps = rand(1,nSampsPerBin);
+  samps = rand(1,ceil(nSampsPerBin));
   samps = samps(samps <= prob);
 
 end
