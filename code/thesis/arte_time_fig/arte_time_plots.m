96a1285d9c95377b6fc0a92bebadd1ac28e5fe68
Greg Hale <imalsogreg@gmail.com>
2015-01-05 18:14:32 -0500

    downsample timing plot to prevent inkscape crash

--- code/thesis/arte_time_fig/arte_time_plots.m
+++ code/thesis/arte_time_fig/arte_time_plots.m
@@ -6,23 +6,25 @@ fastPath = '~/Documents/RetroProject/thesis/rawFigs/decoding.mat';
 load(slowPath);
 ts0 = tsGoal(ts);
 subplot(2,1,1);
-plot(ts0, ts - ts0);
+plotOS(ts0, ts - ts0);
 ylim([0,1]);
 
 load(fastPath);
 ts0 = tsGoal(ts);
 subplot(2,1,2);
-plot(ts0, ts - ts0);
+plotOS(ts0, ts - ts0);
 ylim([0,0.12]);
 end
 
 % Downsample for smaller svg
 % Meh, nevermind
 function plotOS(xs,ys)
-    x  = linspace(xs(1),xs(end),1000);
-    ys = smooth(ys,5);
-    y  = interp1(xs,ys,x);
-    plot(x,y);
+    p = 5;  % 5 percent
+    w = 200;
+    keep = rand(size(xs)) <= p/100;  % keep p % of the points
+    keep(1:w) = 1;                   % also keep the first
+    keep((end-w):end) = 1;            % and last w points
+    plot(xs(keep),ys(keep));
 end
 
 function ts0 = tsGoal(ts)
