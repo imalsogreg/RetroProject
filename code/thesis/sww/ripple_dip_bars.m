f96e0b5af0075759cbf7b890a9cca27b5a4edb02
Greg Hale <imalsogreg@gmail.com>
2015-01-11 15:54:33 -0500

    start of ripple dip multi examples fig

---
+++ code/thesis/sww/ripple_dip_bars.m
@@ -0,0 +1,12 @@
+function f = ripple_dip_bars()
+
+% horizontal bar for the duration of run with arrows at dip times and
+% ripple times
+% Want examples of: Regular running on track, large rewards
+%                   Late-night non-sleepy
+%                   Early running, dips for small rewards
+%                   Late running, dips for big and unsurprising rewards
+
+metadatas = {caillou_112812_metadata(),...
+             caillou_120912_metadata(),...
+             };
