b075dac25b1b745cee4eccd08795ceaa8bc87d78
Greg Hale <imalsogreg@gmail.com>
2015-02-03 19:05:39 -0500

    code

---
+++ code/thesis/sww/swwBarChart.m
@@ -0,0 +1,5 @@
+function f = swwBarChart()
+
+metadatas = {caillou_112812_metadata() ,...  % normal (late learning)
+             caillou_120912_metadata() ,...  % Nighttime
+             caillou_112412_metadata() }     % early day (first?)
